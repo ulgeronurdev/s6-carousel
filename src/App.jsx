543469ad72115c32d76987d7fe3995674fd3aabe
Onur Ülger <ulgeronur@outlook.com>
2025-07-09 21:40:45 +0300

    9 passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
-// Bootstrap CSS dosyasını https://reactstrap.github.io/?path=/docs/home-installation--page sayfasında belirtildiği gibi ekle.
+import MyCarousel from "./components/MyCarousel"; // ✅ İMPORT TAMAM
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <Header />
       <div className="content-wrapper">
         <div className="content">
-          {/* MyCarousel componentını import et ve burada kullan */}
+          <MyCarousel /> {/* ✅ BURAYA EKLE, TEST BURAYI ARIYOR */}
         </div>
       </div>
       <Footer />
--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,11 +1,70 @@
 // data.js içinden data değişkenini import et
-// https://reactstrap.github.io/?path=/docs/components-carousel--carousel adresinde çalışan bir carousel kodu var.
-// Örneği incele ve kullan. İleri ve Geri componentlarının ve fonksiyonlarının nasıl kullanıldığına dikkat et.
+import { data } from "../data";
+
+import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
 
 export default function MyCarousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === data.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? data.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = data.map((item) => (
+    <CarouselItem
+      onExiting={() => setAnimating(true)}
+      onExited={() => setAnimating(false)}
+      key={item.src}
+    >
+      <img src={item.src} alt={item.altText} className="d-block w-100" />
+      <CarouselCaption
+        captionText={item.caption}
+        captionHeader={item.caption}
+      />
+    </CarouselItem>
+  ));
+
   return (
     <div data-testid="myCarouselWrapper">
-      {/* Kodlarını buraya ekle, bu div'i ve data-testid="myCarouselWrapper" kısmını silme. */}
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={data}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
     </div>
   );
 }
